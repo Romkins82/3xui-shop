191db77fcb4ffdc72206c7508d77b670ed3784d1
Romkins82 <romkins82@gmail.com>
2025-10-10 05:10:44 +0700

    revers proxy

--- app/bot/utils/network.py
+++ app/bot/utils/network.py
@@ -22,5 +22,5 @@ async def ping_url(url: str, timeout: int = 5) -> float | None:
 
 def extract_base_url(url: str, port: int, path: str) -> str:
     parsed_url = urlparse(url)
-    base_url = f"{parsed_url.scheme}://{parsed_url.hostname}:{port}"
+    base_url = f"{parsed_url.scheme}://{parsed_url.hostname}"
     return urljoin(base_url, path)
